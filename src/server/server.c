ab1b8a40160e6adf7de4a549a9f1bc50d475c206
DavideB45 <dadewww@gmail.com>
2021-07-16 19:14:23 +0200

    select2

--- src/server/server.c
+++ src/server/server.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 // selfmade
 #include <utils.h>
 #include <server.h>
+#include <FifoList.h>
 
 // connection
 #include <sys/socket.h>
@@ -12,14 +14,17 @@
 #include <sys/select.h>
 #include <unistd.h>
 
-#define SOCKET_FD servGeneral.sockFD
+#define SOCKET_FD srvGen.sockFD
 
-ServerInfo servGeneral;
+
+ServerInfo srvGen;
 
 int readFileConfig(char*);
 int initServer(void);
-void dispatcher(void);
 
+void dispatcher(void);
+//ritorna il massimo FD da ascoltare
+int updatemax(fd_set set, int maxFD);
 
 int main(int argc, char* argv[]){
 
@@ -35,7 +40,14 @@ int readFileConfig(char*);
 
 int initServer(void){
 	
-	unlink(SOCKNAME);
+	// creo una pipe
+	// [1] per scrivere [0] per leggere
+	if(pipe(srvGen.doneReq) == -1){
+		perror("pipe");
+		exit(1);
+	}
+
+	unlink(srvGen.sockName);
 	int sock;
 	SOCKET(sock);
 
@@ -44,7 +56,7 @@ int initServer(void){
 	struct sockaddr_un sa;
 	memset(&sa, 0, sizeof(sa));
 	//inizializzo con i valori giusti
-	strncpy(sa.sun_path, SOCKNAME,UNIX_PATH_MAX);
+	strncpy(sa.sun_path, srvGen.sockName,UNIX_PATH_MAX);
 	sa.sun_family=AF_UNIX;
 				//cast neccessario		
 	if( bind(sock,(struct sockaddr *) &sa, sizeof(sa) ) ){//creo socket
@@ -62,17 +74,28 @@ int initServer(void){
 
 void dispatcher(void){
 	
-	int maxFD = servGeneral.sockFD;
+	int maxFD = srvGen.sockFD > srvGen.doneReq ? srvGen.sockFD : srvGen.doneReq[0];
 	int newConn;
+	int resetConn;
+	int* request;
+	int nReady;
 	fd_set set;
 	fd_set rdSet;
 	FD_ZERO(&set);
 	FD_SET(SOCKET_FD, &set);
 
-	while(servGeneral.serverStatus == S_WORKING){
+	while(srvGen.serverStatus == S_WORKING){
 		rdSet = set;
-		if(select(maxFD + 1, &rdSet, NULL, NULL, NULL) == -1){
+		// se qualcosa non funziona con le interruzioni provare pselect
+		nReady = select(maxFD + 1, &rdSet, NULL, NULL, NULL);
+		if(nReady == -1){
 			/* SELECT ERROR */
+			if(errno != EINTR){
+				exit(1);
+			} else {
+		/* mettere gestione della chiusura corretta in base allo stato del server dopo il segnale */
+				continue;
+			}
 		}
 		for(size_t fd = 0; fd <= maxFD; fd++){
 			if(FD_ISSET(fd, &rdSet)){
@@ -80,15 +103,99 @@ void dispatcher(void){
 					//ritorna un fd del scket che useremo per comunicare
 					newConn = accept(SOCKET_FD ,NULL, 0);
 					if(newConn == -1){
-						perror("accept");
-						
+						/* ACCEPT ERROR */
+						switch(errno){
+						case EBADF:
+							//fd non valido
+							//continuo fino a che ho qualcuno da servire
+							FD_CLR(SOCKET_FD, &set);
+							if(maxFD == SOCKET_FD)
+								maxFD = updatemax(set, maxFD);
+				/* mettere il server in uno stato S_SLOW_CLOSE*/
+						break;
+						case EINTR:
+							/* interrupt */
+		/* mettere gestione della chiusura corretta in base allo stato del server dopo il segnale */
+						break;
+						case EMFILE:
+							/* max file opened */
+							continue;
+						break;
+						default:
+							/* errori gravi */
+							exit(1);
+						break;
+						}
+					} else {
+						if(newConn > 1023){
+							/* fd troppo grande per poll */
+							close(newConn);
+						} else {
+							/* METTO NUOVO CLIENT NELLA MASCHERA */
+							if(maxFD < newConn)
+								maxFD = newConn;
+							FD_SET(newConn, &set);
+						}
 					}
+				} else {
+					if(fd == srvGen.doneReq[0]){
+						/* RESET FROM PIPE */
+						switch( readn(fd, (void*) &resetConn, sizeof(int)) ){
+							case -1:
+								// errore generico
+								// la pipe e' rovinata
+								exit(1);
+							break;
+							case  0:
+								// pipe chiusa (non la guardo piu')
+								close(srvGen.doneReq[0]);
+								FD_CLR(fd, &set);
+							break;
+							case 1:
+								if(IS_TO_RESET(resetConn)){
+									//torno ad ascoltare su quel socket
+									if(maxFD < GET_FD(resetConn))
+										maxFD = GET_FD(resetConn);
+									FD_SET(GET_FD(resetConn), &set);
+								} else {
+									// chiudo il socket
+									FD_CLR(GET_FD(resetConn), &set);
+									close(GET_FD(resetConn));
+								}
+							break;
+						}
+					} else {
+						/* RICHIESTA DA CLIENT */
+						FD_CLR(fd, &set);
+						if(maxFD == fd)
+							maxFD = updatemax(set, maxFD);
+						request = malloc(sizeof(int));
+						if(request == NULL){
+							perror("no mem");
+							exit(1);
+						}
+						*request = fd;
+						if( insert(srvGen.toServe, (void*) request) == 0){
+							printf("Fifo error\n");
+							exit(1);
+						}
+					}
+
 				}
+			
 			}
 			
 		}
 		
+	}	
+	
+}
+
+
+int updatemax(fd_set set, int maxFD){
+	for(int i = (maxFD - 1); i>=0 ; --i ){
+		if(FD_ISSET(i, &set)) 
+			return i;
 	}
-	
-	
+	return -1;
 }
